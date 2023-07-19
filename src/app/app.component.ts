904e8dbd951daa68ea9c0b0a0d1d6636bda6f775
Byron Stearns <bstearns@vizuri.com>
2023-07-19 15:47:25 -0400

    add colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,6 @@ export class AppComponent  {
   constructor(@Inject(DOCUMENT) public doc: Document){
 
 
-
     fetch('assets/data.json').then((res) => res.json())
     .then((data) => {
 
@@ -201,6 +200,34 @@ frame:AddSelectedColumns("Visual impairments")
         labels: graph.map((item: any) => item.disability),
         datasets:[
           {
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
             label: 'Disability',
             data: graph.map((item: any) => item.value)
           }
